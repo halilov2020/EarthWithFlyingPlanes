8f723357e5862838fb6b60299a7091fd94579921
Dmitrii Halilov <d.halilov@unifun.com>
2022-06-05 15:41:56 +0300

    Added rings to the project and made them animated

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { Scene, PerspectiveCamera, WebGLRenderer, DirectionalLight, ACESFilmicToneMapping, sRGBEncoding, PCFSoftShadowMap,
-         Mesh, SphereGeometry, MeshPhysicalMaterial, Color, TextureLoader, PMREMGenerator, FloatType, Group, Vector3, Clock,
-         } 
+         Mesh, SphereGeometry, MeshPhysicalMaterial, Color, TextureLoader, PMREMGenerator, FloatType, Group, Vector2, Vector3,
+         Clock, PlaneGeometry, RingGeometry, DoubleSide} 
     from 'https://cdn.skypack.dev/three@0.137';
 
 import { OrbitControls } from 'https://cdn.skypack.dev/three-stdlib@2.8.5/controls/OrbitControls';
@@ -12,6 +12,11 @@ const scene = new Scene();
 
 const camera = new PerspectiveCamera(45, innerWidth / innerHeight, 0.1, 1000);
 camera.position.set(0, 15, 50);
+
+const ringsScene = new Scene();
+
+const ringsCamera = new PerspectiveCamera(45, innerWidth / innerHeight, 0.1, 1000);
+ringsCamera.position.set(0, 0, 50);
 
 const renderer = new WebGLRenderer({antialias: true, alpha: true});
 renderer.setSize(innerWidth, innerHeight);
@@ -40,7 +45,16 @@ sunLight.shadow.camera.top = 10;
 sunLight.shadow.camera.right = 10;
 scene.add(sunLight);
 
+let mousePos = new Vector2(0,0);
 
+window.addEventListener("mousemove", (e) => {
+    let x = e.clientX - innerWidth * 0.5;
+    let y = e.clientY - innerHeight * 0.5;
+
+    mousePos.x = x * 0.0003;
+    mousePos.y = y * 0.0003;
+
+});
 
 
 (async function(){
@@ -49,6 +63,42 @@ scene.add(sunLight);
     // environment texture am luat de pe https://polyhaven.com/hdris
     let envmapTexture = await new RGBELoader().setDataType(FloatType).loadAsync('textures/old_room_2k.hdr');
     let envMap = pmrem.fromEquirectangular(envmapTexture).texture;
+
+    const ring1 = new Mesh(
+        new RingGeometry(15, 13.5, 80, 1, 0),
+        new MeshPhysicalMaterial({
+            color: new Color('#FFCB8E').convertSRGBToLinear().multiplyScalar(200),
+            roughness: 0.25,
+            envMap,
+            envMapIntensity: 3,
+            side: DoubleSide,
+            transparent: true,
+            opacity: 0.35
+        }),
+    )
+    ringsScene.add(ring1);
+
+    const ring2 = new Mesh(
+        new RingGeometry(16.5, 15.75, 80, 1, 0),
+        new MeshPhysicalMaterial({
+            color: new Color('#FFCB8E').convertSRGBToLinear(),
+            side: DoubleSide,
+            transparent: true,
+            opacity: 0.15
+        }),
+    )
+    ringsScene.add(ring2);
+
+    const ring3 = new Mesh(
+        new RingGeometry(18, 17.75, 80),
+        new MeshPhysicalMaterial({
+            color: new Color('#FFCB8E').convertSRGBToLinear().multiplyScalar(200),
+            side: DoubleSide,
+            transparent: true,
+            opacity: 0.35
+        }),
+    )
+    ringsScene.add(ring3);
 
     let textures = {
         // texturele am luat de pe https://github.com/Domenicobrz/Threejs-in-practice/tree/main/three-in-practice-3/assets
@@ -62,16 +112,6 @@ scene.add(sunLight);
     // "Cartoon Plane" by antonmoek (https://skfb.ly/UOLT)
     let plane = (await new GLTFLoader().loadAsync('textures/plane/scene.glb')).scene.children[0];
     let planesData = [
-        makePlane(plane, textures.planeTrailMask, envMap, scene),
-        makePlane(plane, textures.planeTrailMask, envMap, scene),
-        makePlane(plane, textures.planeTrailMask, envMap, scene),
-        makePlane(plane, textures.planeTrailMask, envMap, scene),
-        makePlane(plane, textures.planeTrailMask, envMap, scene),
-        makePlane(plane, textures.planeTrailMask, envMap, scene),
-        makePlane(plane, textures.planeTrailMask, envMap, scene),
-        makePlane(plane, textures.planeTrailMask, envMap, scene),
-        makePlane(plane, textures.planeTrailMask, envMap, scene),
-        makePlane(plane, textures.planeTrailMask, envMap, scene),
         makePlane(plane, textures.planeTrailMask, envMap, scene),
 
     ]
@@ -88,7 +128,7 @@ scene.add(sunLight);
             sheen: 1,
             sheenRoughness: 0.75,
             sheenColor: new Color('#ff8a00').convertSRGBToLinear(),
-            clearCoat: 0.5
+            // clearCoat: 0.5
         }),
     );
     sphere.rotation.y += Math.PI * 1.25;
@@ -107,7 +147,7 @@ scene.add(sunLight);
             plane.rotation.set(0, 0, 0);
             plane.updateMatrixWorld();
             
-            planeData.rot += delta * 0.25;
+            planeData.rot += delta * 0.25; // decides movement of a plane
             plane.rotateOnAxis(planeData.randomAxis, planeData.randomAxisRot); // random axis
             plane.rotateOnAxis(new Vector3(0, 1, 0), planeData.rot); // y-axis rotation
             plane.rotateOnAxis(new Vector3(0, 0, 1), planeData.rad); // this decides the radius
@@ -118,6 +158,20 @@ scene.add(sunLight);
 
         controls.update();
         renderer.render(scene, camera);
+
+        // ring rotations depending on where mouse is situated in the screen
+        ring1.rotation.x = ring1.rotation.x * 0.95 + mousePos.y * 0.05 * 1.2;
+        ring1.rotation.y = ring1.rotation.y * 0.95 + mousePos.x * 0.05 * 1.2;
+
+        ring2.rotation.x = ring2.rotation.x * 0.95 + mousePos.y * 0.05 * 0.375;
+        ring2.rotation.y = ring2.rotation.y * 0.95 + mousePos.x * 0.05 * 0.375;
+
+        ring3.rotation.x = ring3.rotation.x * 0.95 - mousePos.y * 0.05 * 0.275;
+        ring3.rotation.y = ring3.rotation.y * 0.95 - mousePos.x * 0.05 * 0.275;
+
+        renderer.autoClear = false;
+        renderer.render(ringsScene, ringsCamera);
+        renderer.autoClear = true;
     });
 })();
 
@@ -136,8 +190,26 @@ function makePlane(planeMesh, trailTexture, envMap, scene){
         }
     });
 
+    let trail = new Mesh(
+        new PlaneGeometry(1, 2),
+        new MeshPhysicalMaterial({
+            envMap,
+            envMapIntensity: 3,
+            // color: new Color(1.0, 0.0, 0.5),
+            roughness: 0.4,
+            metalness: 0,
+            transmission: 1,
+            transparent: true,
+            opacity: 1,
+            alphaMap: trailTexture
+        }),
+    );
+    trail.rotateX(Math.PI);
+    trail.translateY(1.1);
+
     let group = new Group();
     group.add(plane);
+    group.add(trail);
 
     scene.add(group);
 
